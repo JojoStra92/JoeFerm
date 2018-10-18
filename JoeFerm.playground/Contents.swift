dbc207995f94bf815a485ca38de6d8c55d882795
Ecole NaN <nan@iMac-05.local>
2018-10-18 13:02:52 +0000

    second commit

--- JoeFerm.playground/Contents.swift
+++ JoeFerm.playground/Contents.swift
@@ -6,64 +6,73 @@ var str = "Hello, Jojo"
 
 
 
-// Le prix du voyage aux caraibes de Joe
-let price:Double = 1499
+/// On ne triche pas ! 
 
-var born: [String: Int]
-born = ["milk": 0, "wheat": 0, "wool": 0]
+// Le prix du voyage
+let price = 1499.0
 
+// L'argent de Joe
 var money = 0.0
-var indexOfday = 0
 
+// Le nombre de jours pendant lesquels Joe doit économiser
+var numberOfDay = 0
+
+// La grange de Joe : [lait, blé, laine]
+var barn = ["milk": 0, "wheat": 0, "wool": 0]
+
+func calculateBarnSize() -> Int {
+    var barnSize = 0
+    
+    for (_, count) in barn {
+        barnSize += count
+    }
+    
+    return barnSize
+}
+
+func feedCows() {
+    money -= 4
+}
+
+func sell() {
+    money += Double(barn["milk"]!) * 0.50
+    money += Double(barn["wheat"]!) * 0.30
+    money += Double(barn["wool"]!) * 1
+    
+    // On vide la grange
+    barn = ["milk": 0, "wheat": 0, "wool": 0]
+}
+
+func harvest() {
+    barn["wheat"]! += 100
+}
+
+func mowSheep() {
+    barn["wool"]! += 30
+}
+
+func milkCows() {
+    barn["milk"]! += 30
+}
 
 while money < price {
-
-    // Joe nourrit ses vaches tous les jours
-    money -= 4
+    feedCows()
     
-    // on calcul la taille de la grange
-    var barnSize = 0
-    for (keys, goods) in born{
-        barnSize += goods
-    }
-    if barnSize >= 500{
-        // on vends !
-        money += Double(born["milk"]!) * 0.50
-        money += Double(born["wheat"]!) * 0.30
-        money += Double(born["wool"]!) * 1
-        
-        // On vide la grange !
-        born = ["milk": 0, "wheat": 0, "wool": 0]
+    if calculateBarnSize() >= 500 {
+        sell()
     } else {
-        
-        if indexOfday % 30 == 1
-        {
-            // joe moisonne
-            born["wheat"]! += 100
-        }
-        else if indexOfday % 30 == 10  || indexOfday % 30 == 20
-        {
-            // Joe tond les moutons
-            born["wool"]! += 30
-        }
-        else
-        {
-            // Joe trait ses vaches
-            born["milk"]! += 30
+        if numberOfDay % 30 == 1 {
+            harvest()
+        } else if numberOfDay % 30 == 10 || numberOfDay % 30 == 20 {
+            mowSheep()
+        } else {
+            milkCows()
         }
     }
     
-    indexOfday += 1
+    // On passe au jour suivant
+    numberOfDay += 1
 }
-print("Il aura fllu \(indexOfday) jours a Joe pour économiser \(money)€")
 
 
-
-
-
-
-
-
-
-
-
+print("Il aura fallu \(numberOfDay) jours à Joe pour économiser \(money) €")
